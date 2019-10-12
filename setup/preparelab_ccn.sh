88e254f170249a066bb4365647b7080bb6145626
Daniel Oh <doh@danieloh.redhat.com>
2019-10-12 19:20:56 -0400

    readjust knative-serving view setting

--- setup/preparelab_ccn.sh
+++ setup/preparelab_ccn.sh
@@ -226,10 +226,6 @@ if [ -z "${MODULE_TYPE##*m4*}" ] ; then
  
   echo -e "Installing Knative Eventing..."
   oc apply -f https://raw.githubusercontent.com/RedHat-Middleware-Workshops/cloud-native-workshop-v2-infra/ocp-4.1/files/knative-eventing-subscription.yaml
- 
-  for i in $(eval echo "{0..$USERCOUNT}") ; do
-    oc adm policy add-role-to-user view user$i -n knative-serving
-  done
 
 echo -e "Creating Role, Group, and assign Users"
 for i in $(eval echo "{0..$USERCOUNT}") ; do
@@ -400,6 +396,10 @@ for i in $(eval echo "{0..$USERCOUNT}") ; do
   oc delete limitranges user0-cloudnativeapps-core-resource-limits -n user$i-cloudnativeapps
   oc delete limitranges user$i-cloudnative-pipeline-core-resource-limits -n user$i-cloudnative-pipeline
   oc adm policy add-role-to-user admin user$i -n user$i-cloudnative-pipeline
+done
+
+for i in $(eval echo "{0..$USERCOUNT}") ; do
+  oc adm policy add-role-to-user view user$i -n knative-serving
 done
 
 fi
